d6efd0d19bea0ced213847bb396f1b23760a6252
dchima <dcnnad@gmail.com>
2019-11-14 19:03:06 +0100

    gh pages test

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import App from './pages/app';
+import { HashRouter, Route, Redirect } from 'react-router-dom';
+import App from 'pages/app';
 import ErrorPage from 'pages/404';
 
 class Main extends Component {
   render() {
     return (
-      <Switch>
-        <Route path='/home' component={App} />
-      </Switch>
+      <HashRouter basename='/'>
+        <Route exact path='/' component={App} />
+        <Route exact path='/404' component={ErrorPage} />
+        <Redirect to="/404" />
+      </HashRouter>
     );
   }
 }
