5cf095991be506324af3e08dc415b9561dc085ad
dchima <dcnnad@gmail.com>
2019-11-14 18:46:33 +0100

    testing gh-pages

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import App from 'pages/app';
+import App from './pages/app';
 import ErrorPage from 'pages/404';
 
 class Main extends Component {
   render() {
     return (
       <Switch>
-        <Route path='/' exact component={App} />
+        <Route path='/home' component={App} />
       </Switch>
     );
   }
