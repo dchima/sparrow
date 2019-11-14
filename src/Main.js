40f69b84c2b602485fd0118f8dee5d4357f6f124
dchima <dcnnad@gmail.com>
2019-11-14 19:11:00 +0100

    router testing

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import App from 'pages/app';
 import ErrorPage from 'pages/404';
 
 class Main extends Component {
   render() {
     return (
-      <HashRouter basename='/'>
+      <Switch>
         <Route exact path='/' component={App} />
         <Route exact path='/404' component={ErrorPage} />
         <Redirect to="/404" />
-      </HashRouter>
+      </Switch>
     );
   }
 }
