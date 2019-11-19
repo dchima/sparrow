31e8c989b4d9955536e3735bcf78aad5f5044990
dchima <dcnnad@gmail.com>
2019-11-19 15:45:31 +0100

    small edits

--- src/data/content.js
+++ src/data/content.js
@@ -5,7 +5,7 @@ const content = {
   desc2: 'Let’s build together.',
   about: {
     body1: 'Hey there! I’m Chima. I’m a software engineer based in lagos, Nigeria. I spend most of my time building and designing great applications for the web. People with great ideas are all around us, waiting for the right tools to help move the world forward. I’m the guy that spends all his time helping others build those tools. Here are a few of my alma maters',
-    body2: 'After graduating from the University of Leeds, I started work with Zicli Synergy, where I head and manage their technology department. I am currently working with Fourshores Inc, where I help develop useful tools and products ready for the Nigerian Market.',
+    body2: 'After graduating from the University of Leeds, I started work with Zicli Synergy, where I head and manage their technology department. I am currently working with Fourshores Inc, where I help develop useful tools and products ready for the Nigerian Market. Below are a few of the tools I use to acheive my goals:',
 
   },
 };
